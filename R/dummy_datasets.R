fc3afa893ef6fe2f3add378514678fe6269ac918
OlisaNsonwu <47105194+OlisaNsonwu@users.noreply.github.com>
2019-09-10 18:07:56 +0100

    added some dummy datasets to the package
    added some dummy datasets to the package

---
+++ R/dummy_datasets.R
@@ -0,0 +1,19 @@
+#' @title Dummy data for \code{diyar}
+#' @description Dummy staff record with some missing data
+#' @docType data
+#' @usage data(staff_records)
+#' @format tibble
+#' @keywords datasets
+#' @examples
+#' data(staff_records)
+"staff_records"
+
+#' @title Dummy data for \code{diyar}
+#' @description Dummy staff records with missing staff identifiers
+#' @docType data
+#' @usage data(missing_staff_id)
+#' @format tibble
+#' @keywords datasets
+#' @examples
+#' data(missing_staff_id)
+"missing_staff_id"
