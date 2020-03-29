bed1aa1e3472d9334db6f335726a97c0d276a9fd
Olisa <olisa.nsonwu@gmail.com>
2020-03-29 00:03:44 +0000

    Update episode_group.R
    improved xlims

--- R/episode_group.R
+++ R/episode_group.R
@@ -937,7 +937,8 @@ plot_epid <- function(epid, date= NULL, strata = NULL, case_length = NULL, recur
   story <- function(dfp){
     #x-axis limits
     # min and dates plus & minus lengths
-    xlims <- c((min(dfp$dt_a) - max(c(max(dfp$c), max(dfp$r)))), (max(dfp$dt_z) + max(c(max(dfp$c), max(dfp$r)))))
+    #xlims <- c((min(dfp$dt_a) - max(c(max(dfp$c), max(dfp$r)))), (max(dfp$dt_z) + max(c(max(dfp$c), max(dfp$r)))))
+    xlims <- c(min(dfp$dt_a)-1, max(dfp$dt_z)+1)
 
     # Colors groups for windows
     dfp$win_cols <- rev(pl_cols)[as.numeric(as.factor(dfp$wind_id))]
@@ -1081,7 +1082,11 @@ plot_epid <- function(epid, date= NULL, strata = NULL, case_length = NULL, recur
       for(i in 1:nrow(cl)){
         # Surpressed warning from 0 length arrows
         suppressWarnings(graphics::arrows(length=0.1,angle=20, y0=cl$case_len_y_axis[i], x0 = cl$dt[i], x1 = cl$end_dt[i], col ="white"))
-        graphics::text(cex = .7 * scale_fac, y=cl$case_len_y_axis[i] + (scale_fac * 0.02) , x=mean(c(cl$dt[i], cl$end_dt[i])), labels = cl$lab[i], col ="white", adj =c(.5,0))
+
+        if(i == nrow(cl) & i != 1 & dfp$dt_z[nrow(dfp)] < cl$end_dt[i]) x_pos <- if_else(from_last ==T, cl$dt[i] + (scale_fac * 0.5), cl$dt[i] - (scale_fac * 0.5))
+        else x_pos <- mean(c(cl$dt[i], cl$end_dt[i]))
+
+        graphics::text(cex = .7 * scale_fac, y=cl$case_len_y_axis[i] + (scale_fac * 0.02) , x=x_pos, labels = cl$lab[i], col ="white", adj =c(0.5 ,0))
         graphics::lines(y=c(cl$case_len_y_axis[i] - (scale_fac * 0.015), cl$case_len_y_axis[i] + (scale_fac * 0.015)), x = rep(cl$dt[i],2), col=cl$win_col[i])
 
         # Trying to guess when bi_direction has been used.
@@ -1108,8 +1113,12 @@ plot_epid <- function(epid, date= NULL, strata = NULL, case_length = NULL, recur
 
       for(i in 1:nrow(rl)){
         # Surpressed warning from 0 length arrows
+
+        if(i == nrow(rl) & i != 1 & dfp$dt_z[nrow(dfp)] < rl$end_dt[i]) x_pos <- if_else(from_last ==T, rl$dt[i] + (scale_fac * 0.5), rl$dt[i] - (scale_fac * 0.5))
+        else x_pos <- mean(c(rl$dt[i], rl$end_dt[i]))
+
         suppressWarnings(graphics::arrows(length=0.1,angle=20, y0=rl$rec_len_y_axis[i], x0 = rl$dt[i], x1 = rl$end_dt[i], lty=2, col ="white"))
-        graphics::text(cex = .7 * scale_fac, y=rl$rec_len_y_axis[i] + (scale_fac * 0.02) , x=mean(c(rl$dt[i], rl$end_dt[i])), labels = rl$lab[i], col ="white", adj =c(.5,0))
+        graphics::text(cex = .7 * scale_fac, y=rl$rec_len_y_axis[i] + (scale_fac * 0.02) , x= x_pos, labels = rl$lab[i], col ="white", adj =c(.5,0))
         graphics::lines(y=c(rl$rec_len_y_axis[i] - (scale_fac * 0.015), rl$rec_len_y_axis[i] + (scale_fac * 0.015)), x = rep(rl$dt[i],2), col=rl$win_col[i])
       }
 
